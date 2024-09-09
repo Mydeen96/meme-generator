00ac4616e3ae19c21ca8c4eda5450a7ec0ae9b45
Mydeen <Mydeen1193@gmail.com>
2024-09-09 12:30:39 +0530

    Create header

---
+++ src/Components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header(){
+    return(
+        <nav className="navbar">
+            <img src="/public/images/troll-face.png" alt="Troll Face image" />
+            <h2>Meme Generator</h2>
+            <h3>React - Project</h3>
+        </nav>
+    )
+}
---
+++ src/Components/Memepage.jsx
@@ -0,0 +1,6 @@
+
+export default function Memepage(){
+    <div className="content">
+        
+    </div>
+}
