8c646dd29c5fa4be3042dd7b998f5352a4400c64
Mydeen <Mydeen1193@gmail.com>
2024-09-13 20:30:37 +0530

    add header name

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header(){
     return(
         <nav className="navbar">
-            <img src="/public/images/troll-face.png" alt="Troll Face image" />
+            <img src="./images/troll-face.png" alt="Troll Face image" />
             <h2>Meme Generator</h2>
             <h3>React - Project</h3>
         </nav>
